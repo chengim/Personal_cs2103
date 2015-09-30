1ddd6d2177b732885f4dc9eba0f06ae0bd2d4574
chengim <chengim@users.noreply.github.com>
2015-10-01 00:31:38 +0800

    update methods ADD, DELETE, UPDATE

--- src/MemoriCalendar.java
+++ src/MemoriCalendar.java
@@ -3,18 +3,27 @@ package memori;
 import java.util.ArrayList;
 
 public class MemoriCalendar {
+	private static final String MESSAGE_ADD = "adding";
+	private static final String MESSAGE_DELETE = "deleting\n";
+	private static final String MESSAGE_UPDATE = "updating";
+	private static final String LINE_INDEX_DOES_NOT_EXISTS = "Line index does not exists\n";
+	private static final String MESSAGE_EMPTYFILE = "File is Empty.\n";
+	
 	private ArrayList<MemoriEvent>  memoriCalendar;
-	private static final String HEADER ="Name of Event:       Start:       End:\n";
+	//added location/description/priority
+	private static final String HEADER ="Name of Event:    Start:    End:    Location:    Description:    Priority:\n";
 	
 	
 	public MemoriCalendar(){
 		memoriCalendar = new ArrayList<MemoriEvent>();
 	}
 	public void add(MemoriCommand command){
-		MemoriEvent event = new MemoriEvent(command.getName(),command.getStart(),command.getEnd());
-		System.out.println("adding");
+		MemoriEvent event = new MemoriEvent(command.getName(),command.getStart(),command.getEnd(),
+					command.getInternalId(), command.getExternalId(), command.getDescription());
+		System.out.println(MESSAGE_ADD);
 		memoriCalendar.add(event);
 	}
+	//What about priority/location? What is the difference between internalId and externalId?
 	
 	public String display(){
 		String output = HEADER;	
@@ -38,11 +47,44 @@ public class MemoriCalendar {
 		
 	}
 	private void delete(MemoriCommand command) {
-		// TODO Auto-generated method stub
+		memoriCalendar.display();
+		String deleteText;
+			if(memoriCalendar.isEmpty()){
+				System.out.println(MESSAGE_EMPTYFILE);
+			}else{
+				int index = Integer.parseInt(command.getExternalCalId());
+				//to implement getExternalCalId in MemoriCommand that returns a String
+				
+				if(memoriCalendar.size() < index){
+					System.out.println(LINE_INDEX_DOES_NOT_EXISTS);	
+				}else{
+					deleteText = memoriCalendar.get(index - 1);
+					memoriCalendar.remove(index - 1);
+					memoriCalendar.trimToSize();
+					System.out.println(MESSAGE_DELETE + deleteText);	
+				}
+	
+			}
+
 		
 	}
 	private void update(MemoriCommand command) {
-		// TODO Auto-generated method stub
+		memoriCalendar.display();
+			if(memoriCalendar.isEmpty()){
+				System.out.println(MESSAGE_EMPTYFILE);
+			}else{
+				int index = Integer.parseInt(command.getExternalCalId());
+				
+				if(memoriCalendar.size() < index){
+					System.out.println(LINE_INDEX_DOES_NOT_EXISTS);
+				}else{
+					originalEvent = memoriCalendar.get(index - 1);
+					MemoriEvent eventUpdate = originalEvent.update(command.getName(),
+								command.getStart(), command.getEnd(), command.getDescription());
+					System.out.println(MESSAGE_UPDATE);
+					memoriCalendar.set(eventUpdate);
+				}
+			}
 		
 	}
 }
